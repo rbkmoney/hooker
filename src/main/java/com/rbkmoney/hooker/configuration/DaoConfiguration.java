511ae60f8e79e09715afb343ea3ce80113e9a6b6
Inal Arsanukaev <inalarsanukaev@MacBook-Pro-Inal.local>
2017-07-12 17:26:48 +0300

    HOOK-33: up damsel version

--- src/main/java/com/rbkmoney/hooker/configuration/DaoConfiguration.java
+++ src/main/java/com/rbkmoney/hooker/configuration/DaoConfiguration.java
@@ -8,8 +8,6 @@ import com.rbkmoney.hooker.dao.impl.HookDaoImpl;
 import com.rbkmoney.hooker.dao.impl.MessageDaoImpl;
 import com.rbkmoney.hooker.dao.impl.SimpleRetryPolicyDaoImpl;
 import com.rbkmoney.hooker.dao.impl.TaskDaoImpl;
-import org.jooq.Schema;
-import org.jooq.impl.SchemaImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.DependsOn;
@@ -42,10 +40,5 @@ public class DaoConfiguration {
     @DependsOn("dbInitializer")
     public SimpleRetryPolicyDao simpleRetryPolicyDao(DataSource dataSource) {
         return new SimpleRetryPolicyDaoImpl(dataSource);
-    }
-
-    @Bean
-    public Schema dbSchema() {
-        return new SchemaImpl("hook");
     }
 }
