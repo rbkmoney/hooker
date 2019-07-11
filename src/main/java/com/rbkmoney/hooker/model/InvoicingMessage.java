291a05c309e2bf5f1277f0396a934295c046b4c4
Inal Arsanukaev <inalarsanukaev@MacBook-Pro-Inal.local>
2019-07-11 16:57:50 +0300

    Removed unused copy constructor

--- src/main/java/com/rbkmoney/hooker/model/InvoicingMessage.java
+++ src/main/java/com/rbkmoney/hooker/model/InvoicingMessage.java
@@ -24,26 +24,6 @@ public class InvoicingMessage extends Message {
     private Payment payment;
     private Refund refund;
 
-    public InvoicingMessage(InvoicingMessage other) {
-        setId(other.getId());
-        this.eventId = other.eventId;
-        this.sequenceId = other.sequenceId;
-        this.changeId = other.changeId;
-        this.eventTime = other.eventTime;
-        this.type = other.type;
-        this.partyId = other.partyId;
-        this.eventType = other.eventType;
-        if (other.invoice != null) {
-            this.invoice = new Invoice(other.invoice);
-        }
-        if (other.payment != null) {
-            this.payment = new Payment(other.payment);
-        }
-        if (other.refund != null) {
-            this.refund = new Refund(other.refund);
-        }
-    }
-
     public boolean isInvoice() {
         return AbstractInvoiceEventHandler.INVOICE.equals(getType());
     }
@@ -54,16 +34,6 @@ public class InvoicingMessage extends Message {
 
     public boolean isRefund() {
         return AbstractInvoiceEventHandler.REFUND.equals(getType());
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        InvoicingMessage message = (InvoicingMessage) o;
-
-        return getId() == message.getId();
     }
 
     @Override
@@ -84,7 +54,4 @@ public class InvoicingMessage extends Message {
         return (int) (getId() ^ (getId() >>> 32));
     }
 
-    public InvoicingMessage copy(){
-        return new InvoicingMessage(this);
-    }
 }
