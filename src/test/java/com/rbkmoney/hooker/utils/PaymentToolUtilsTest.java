cf8105d12e9cd8e2046c5f38ff9bcb850fcf051a
vbanin <vitaxa93gamebox@gmal.com>
2019-07-30 19:02:28 +0300

    add payment fee

--- src/test/java/com/rbkmoney/hooker/utils/PaymentToolUtilsTest.java
+++ src/test/java/com/rbkmoney/hooker/utils/PaymentToolUtilsTest.java
@@ -2,13 +2,13 @@ package com.rbkmoney.hooker.utils;
 
 import com.rbkmoney.damsel.domain.CryptoCurrency;
 import com.rbkmoney.damsel.domain.PaymentTool;
-import com.rbkmoney.swag_webhook_events.PaymentToolDetails;
-import com.rbkmoney.swag_webhook_events.PaymentToolDetailsBankCard;
-import com.rbkmoney.swag_webhook_events.PaymentToolDetailsCryptoWallet;
+import com.rbkmoney.swag_webhook_events.model.PaymentToolDetails;
+import com.rbkmoney.swag_webhook_events.model.PaymentToolDetailsBankCard;
+import com.rbkmoney.swag_webhook_events.model.PaymentToolDetailsCryptoWallet;
 import org.junit.Test;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class PaymentToolUtilsTest {
 
@@ -17,7 +17,7 @@ public class PaymentToolUtilsTest {
         PaymentTool paymentTool = PaymentTool.crypto_currency(CryptoCurrency.bitcoin);
         PaymentToolDetails paymentToolDetails = PaymentToolUtils.getPaymentToolDetails(paymentTool);
         assertEquals(PaymentToolDetails.DetailsTypeEnum.PAYMENTTOOLDETAILSCRYPTOWALLET, paymentToolDetails.getDetailsType());
-        assertEquals(com.rbkmoney.swag_webhook_events.CryptoCurrency.BITCOIN.getValue(), ((PaymentToolDetailsCryptoWallet)paymentToolDetails).getCryptoCurrency().getValue());
+        assertEquals(com.rbkmoney.swag_webhook_events.model.CryptoCurrency.BITCOIN.getValue(), ((PaymentToolDetailsCryptoWallet)paymentToolDetails).getCryptoCurrency().getValue());
     }
 
     @Test
@@ -34,7 +34,7 @@ public class PaymentToolUtilsTest {
     @Test
     public void testSetPaymentToolDetailsParam() {
         MapSqlParameterSource params = new MapSqlParameterSource();
-        PaymentToolDetails paymentToolDetails = new PaymentToolDetailsCryptoWallet().cryptoCurrency(com.rbkmoney.swag_webhook_events.CryptoCurrency.BITCOIN);
+        PaymentToolDetails paymentToolDetails = new PaymentToolDetailsCryptoWallet().cryptoCurrency(com.rbkmoney.swag_webhook_events.model.CryptoCurrency.BITCOIN);
         paymentToolDetails.setDetailsType(PaymentToolDetails.DetailsTypeEnum.PAYMENTTOOLDETAILSCRYPTOWALLET);
         PaymentToolUtils.setPaymentToolDetailsParam(params, paymentToolDetails,
                 "detailsTypeParamName", "binParamName", "lastDigitsParamName", "cardNumberMaskParamName", "tokenProviderParamName",
