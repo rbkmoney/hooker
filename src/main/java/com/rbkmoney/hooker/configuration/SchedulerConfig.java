b44c5a115c9856c4e4e66dd8b8517927f9aa2de1
Inal Arsanukaev <inalarsanukaev@MacBook-Pro-Inal.local>
2020-11-18 17:42:59 +0300

    Extract property

--- src/main/java/com/rbkmoney/hooker/configuration/SchedulerConfig.java
+++ src/main/java/com/rbkmoney/hooker/configuration/SchedulerConfig.java
@@ -1,17 +1,17 @@
 package com.rbkmoney.hooker.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 
 @Configuration
 public class SchedulerConfig {
-    private static final int SCHEDULER_POOL_SIZE = 3;
 
     @Bean
-    public ThreadPoolTaskScheduler taskScheduler() {
+    public ThreadPoolTaskScheduler taskScheduler(@Value("${scheduling.poolSize}") int schedullerPoolSize) {
         ThreadPoolTaskScheduler taskScheduler = new ThreadPoolTaskScheduler();
-        taskScheduler.setPoolSize(SCHEDULER_POOL_SIZE);
+        taskScheduler.setPoolSize(schedullerPoolSize);
         return  taskScheduler;
     }
 }
